f71369069498a04f88963d9a6060dc2a0b2d0758
jfgc <jf.gc@outlook.pt>
2026-07-10 19:57:35 +0100

    Migração Inicial

--- JoinIt/Program.cs
+++ JoinIt/Program.cs
@@ -1,5 +1,5 @@
 using JoinIt.Data;
-using Microsoft.AspNetCore.Identity;
+using JoinIt.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
