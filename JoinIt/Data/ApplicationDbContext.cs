31ff7d65b676b39f7af04e6176dca3d070cf1265
jfgc <jf.gc@outlook.pt>
2026-07-10 19:16:09 +0100

    Definição do DBContext

--- JoinIt/Data/ApplicationDbContext.cs
+++ JoinIt/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using JoinIt.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace JoinIt.Data
 {
@@ -8,6 +10,65 @@ namespace JoinIt.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Evento> Eventos { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Participante> Participantes { get; set; }
+
+        public DbSet<Mensagem> Mensagens { get; set; }
+
+        public DbSet<Amizade> Amizades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configuração da relação 
+            builder.Entity<Evento>()
+                .HasOne(e => e.Criador)
+                .WithMany(u => u.EventosCriados)
+                .HasForeignKey(e => e.CriadorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Evento>()
+                .HasOne(e => e.Categoria)
+                .WithMany(c => c.Eventos)
+                .HasForeignKey(e => e.CategoriaId);
+
+            builder.Entity<Participante>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.EventosParticipados)
+                .HasForeignKey(p => p.UserId);
+
+            builder.Entity<Participante>()
+                .HasOne(p => p.Evento)
+                .WithMany(e => e.Participantes)
+                .HasForeignKey(p => p.EventoId);
+
+            builder.Entity<Mensagem>()
+                .HasOne(m => m.User)
+                .WithMany(u => u.Mensagens)
+                .HasForeignKey(m => m.UserId);
+
+            builder.Entity<Mensagem>()
+                .HasOne(m => m.Evento)
+                .WithMany(e => e.Mensagens)
+                .HasForeignKey(m => m.EventoId);
+
+            builder.Entity<Amizade>()
+                .HasOne(a => a.PedidoPor)
+                .WithMany(u => u.PedidosEnviados)
+                .HasForeignKey(a => a.PedidoPorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Amizade>()
+                .HasOne(a => a.PedidoA)
+                .WithMany(u => u.PedidosRecebidos)
+                .HasForeignKey(a => a.PedidoAId)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }
